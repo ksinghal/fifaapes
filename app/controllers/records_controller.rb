fd94df31551e6ee4d7871cebc0d0b6087d8ea01b
glasslighttech <ksinghal@thrupoint.com>
2013-10-28 21:49:55 -0400

    updated

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,6 +2,17 @@ class RecordsController < ApplicationController
   def index
     @players = RegisteredPlayer.scoped
     @games = Game.scoped
+    @players.each do |player|
+      if (( @games.where(winner: player.name).count + @games.where(loser: player.name).count ) <= 0)
+        player.winning_pct = 0
+        puts "WINNING PCT IS 0 BECAUSE NO GAMES PLAYED!!!"
+      else
+        player.winning_pct = @games.where(winner: player.name).count / ( @games.where(winner: player.name).count + @games.where(loser: player.name).count )
+
+        puts "WINNING PCT IS NOW LOGGED SUCCESS!!!"
+      end
+      player.save
+    end
   end
   def new
     @players = RegisteredPlayer.all
