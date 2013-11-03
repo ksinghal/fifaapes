084e0a1c72f0620fa8f2649f523790384761c47b
glasslighttech <ksinghal@thrupoint.com>
2013-11-02 21:19:56 -0400

    fixed issue with wining pct

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,8 @@ class PagesController < ApplicationController
   end
   def submit_registration
     @player = RegisteredPlayer.create(params[:registered_player])
+    @player.winning_pct = 0.to_f
+    @player.save
     RegistrationMailer.new_user_email(@player).deliver
   end
   def constitution
